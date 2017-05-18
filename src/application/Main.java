f24267febfc8517fb3251b2c8e68590ecb07c491
StraetenMax <maxstraeten@yahoo.com>
2017-05-18 13:23:39 +0200

    creation Main

---
+++ src/application/Main.java
@@ -0,0 +1,19 @@
+package application;
+
+import tPcnam.Personnage;
+import tPcnam.PersonnageJoueur;
+import tPcnam.PersonnageJoueurGuerrier;
+import tPcnam.PersonnageJoueurMedecin;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//instancier les classes
+		Personnage P1 = new Personnage("Humain",180,75);
+		PersonnageJoueur PJ1 = new PersonnageJoueur("Nain",100,90,"");
+		PersonnageJoueurGuerrier PJG1 = new PersonnageJoueurGuerrier("Orc",200,180,"Alain",40,100,true);
+		PersonnageJoueurGuerrier PJG2 = new PersonnageJoueurGuerrier("Elfe",190, 90,"Jean", 25, 100,true);		
+		PersonnageJoueurMedecin PJM1 = new  PersonnageJoueurMedecin("Humain", 175 , 65,"Pierre", 20);
+		PersonnageJoueurMedecin PJM2 = new PersonnageJoueurMedecin("Elfe", 185, 80,"Jacques", 26);
+	}
+}
