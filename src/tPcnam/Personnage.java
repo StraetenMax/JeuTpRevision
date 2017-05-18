6b569d4857805acd14922efa3c8e02d0631ca269
StraetenMax <maxstraeten@yahoo.com>
2017-05-18 10:25:01 +0200

    methodes classe Personnage

--- src/tPcnam/Personnage.java
+++ src/tPcnam/Personnage.java
@@ -16,5 +16,37 @@ public class Personnage {
 		this.taille = taille;
 		this.poids = poids;																		
 	}
-
+	
+	//accesseurs et mutateurs
+	public String getRace(){
+		return race;
+	}
+	public int getTaille(){
+		return taille;
+	}
+	public int getPoids(){
+		return poids;
+	}
+	public void setRace(String race){
+		this.race = race;	
+	}
+	public void setTaille(int taille){
+		this.taille = taille;
+	}
+	public void setPoids(int poids){
+		this.poids= poids;
+	}
+	
+	//méthodes
+	public String marcher(){
+		return "Personnage marche";
+	}
+	public String dormir(){
+		String str = "Personnage dort";
+		return str;
+	}
+	public String description(){
+		String str1 ="Personnage de race "+this.getRace()+" qui mesure "+this.getTaille()+" cm et pèse "+this.getPoids()+" kg";
+		return str1;
+	}
 }
